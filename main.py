abc62da7307043720fbd712db7e9ca237d53988c
shaitansix <shaitansix3@gmail.com>
2025-02-26 13:26:05 -0500

    Configuracion vercel

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ from fastapi import FastAPI
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
 from routes.imageRoute import router as imageRouter
+from schemas.responseSchema import responseEntity
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = ['http://localhost', 'http://localhost:5173'], 
+    allow_origins = ['http://localhost', 'http://localhost:5173', 'https://ocr-frontend-phi.vercel.app'], 
     allow_credentials = True,
     allow_methods = ['*'], 
     allow_headers = ['Content-Type']
 )
+
 app.include_router(imageRouter)
+@app.get('/{full_path:path}')
+async def not_found(full_path: str):
+    return responseEntity(
+        state = 'Success', 
+        message = 'La ruta no existe'
+    )
 
 if __name__ == '__main__': 
     port = 8000
