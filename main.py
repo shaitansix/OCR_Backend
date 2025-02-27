627cea667299d8ef50fed573a3dfac7d0845413e
shaitansix <shaitansix3@gmail.com>
2025-02-26 21:58:56 -0500

    Configurando vercel

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
 from fastapi import FastAPI
-import uvicorn
-import os
-from dotenv import load_dotenv
 from routes.imageRoute import router as imageRouter
 from routes.notFound import router as notFoundRouter
 from config.cors import corsSetup
 
 app = FastAPI()
-load_dotenv('.env')
 corsSetup(app)
 
 app.include_router(imageRouter)
 app.include_router(notFoundRouter)
-
-if __name__ == '__main__': 
-    PORT = int(os.getenv('PORT', 8000))
-    uvicorn.run(app, port = PORT)
