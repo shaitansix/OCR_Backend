7e579325d40edef91e80095c05f66cd34080bea3
shaitansix <shaitansix3@gmail.com>
2025-02-26 21:37:41 -0500

    Configurando variables de entorno

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 import uvicorn
+from dotenv import load_dotenv
+import os
 from routes.imageRoute import router as imageRouter
 from routes.notFound import router as notFoundRouter
 from config.cors import corsSetup
 
 app = FastAPI()
+load_dotenv('.env')
 corsSetup(app)
 
 app.include_router(imageRouter)
 app.include_router(notFoundRouter)
 
 if __name__ == '__main__': 
-    port = 8000
-    uvicorn.run(app, port = port)
+    PORT = int(os.getenv('PORT', 8000))
+    uvicorn.run(app, port = PORT)
